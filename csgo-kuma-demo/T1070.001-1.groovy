0cf3532f856fb2719a7896d0270e064fc6cc817c
Alexander Komissarov <fatalick@gmail.com>
2024-03-08 01:30:29 +0300

    Create T1070.001-1.groovy

---
+++ csgo-kuma-demo/T1070.001-1.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent {label 'windows'}
+
+    stages {
+        stage('Run KUMA Atomic T1070.001-1: R050_01_Очистка журнала событий Windows через командную строку using wevtutil') {
+            steps {
+                powershell '''
+					Invoke-AtomicTest T1070.001 -PathToAtomicsFolder "C:\\kuma-atomics" -TestNumbers 1
+					'''
+            }
+        }
+        stage('Recive alert from KUMA: R050_01'){
+            options {
+                timeout(time: 1, unit: "MINUTES")
+            }
+            steps {
+                script {
+                    hook = registerWebhook(token: "webhook")
+                    callbackURL = hook.url
+                    data = waitForWebhook hook
+                }
+            }
+        }
+    }
+}
